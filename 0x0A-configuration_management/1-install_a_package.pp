9fcc34a2f7111e984432994c6ca3409a979fb8a6
Hamoodteen <ahmed104567@outlook.com>
2024-01-19 13:54:01 +0200

    0

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
-#!/usr/bin/pup
-# Install an especific version of flask (2.1.0)
-package {'flask':
+# create_a_file.pp
+
+package { 'flask':
   ensure   => '2.1.0',
-  provider => 'pip3'
+  provider => 'pip3',
 }
