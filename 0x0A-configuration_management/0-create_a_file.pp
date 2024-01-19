28e2d8a7043f804512c1c8492e2c429d14ff6bc4
Hamoodteen <ahmed104567@outlook.com>
2024-01-19 13:38:41 +0200

    0

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,7 @@
-file { 'school':
-  path       => '/tmp/school',
-  permission => '0744',
-  owner      => 'www-data',
-  group      => 'www-data',
-  contain    => 'I love Puppet',
+file { '/tmp/school':
+  ensure  => file,
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
 }
