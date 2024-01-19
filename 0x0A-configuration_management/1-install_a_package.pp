c4b0cdd772e9ee6320596e89ffd52df8f9dbb75c
Hamoodteen <ahmed104567@outlook.com>
2024-01-19 13:52:24 +0200

    0

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,13 +1,6 @@
-# create_a_file.pp
+# commentttttttttttttttttttttttttttttttttt
 
 package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
-  require  => Exec['update_pip3'], # Optional: Ensure pip3 is up-to-date before installing Flask
-}
-
-exec { 'update_pip3':
-  command     => 'pip3 install --upgrade pip',
-  refreshonly => true,
-  path        => ['/usr/local/bin', '/usr/bin'],
 }
