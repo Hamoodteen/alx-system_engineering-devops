65fe9143cd2713db222b9c94787a6534643c381a
Hamoodteen <ahmed104567@outlook.com>
2024-03-06 01:07:58 +0200

    0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -3,13 +3,12 @@
 import requests
 
 
-def get_subreddit_subscribers(subreddit_name):
+def number_of_subscribers(subreddit_name):
     url = f'https://www.reddit.com/r/{subreddit_name}/about.json'
-    headers = {'User-Agent': 'your_user_agent_here'}
-    response = requests.get(url, headers=headers)
+    response = requests.get(url)
     if response.status_code == 200:
         data = response.json()
         subscribers = data['data']['subscribers']
         return subscribers
     else:
-        return None
+        return 0
